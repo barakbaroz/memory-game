915fdcde13ccc1962374d68fcd5ba071e66f4267
Barak <barozbarak@gmail.com>
2022-08-19 01:32:08 +0300

    the real game

--- ww.js
+++ ww.js
@@ -1,4 +1,5 @@
 
+  let num
 let game_st= {
   Color_order:[],
   Color_order_player:[],
@@ -8,26 +9,26 @@ let game_st= {
   let top_left_c={
     Initial_color:'#5286ad',
     highlight_color:'#0013ff',
-    value:'a',
+    value:1,
     name:"top-left-circle clear"
   }
   let top_right_c={
     Initial_color:'#ac5359',
     highlight_color:'#ff0003',
-    value:'b',
+    value:2,
     name:"top-right-circle"
     
   }
   let bottom_left_c={
     Initial_color:'#59a67f',
     highlight_color:'#00ff74',
-    value:'c',
+    value:3,
     name:"bottom-left-circle"
   }
   let bottom_right_c={
     Initial_color:'#c4c639',
     highlight_color:'#fff000',
-    value:'d',
+    value:4,
     name:"bottom-right-circle"
   } 
   let start=document.querySelector('.btn-alt')
@@ -44,7 +45,6 @@ let game_st= {
             },1000 ) 
             setTimeout(()=>{
              b.style.background=a.Initial_color  
-             game_st.Color_order.push(a.value) 
           resolve(game_st)
            },1700 )   
           })
@@ -61,53 +61,62 @@ let game_st= {
      b_r.addEventListener('click',()=>{ 
       click_player(b_r,bottom_right_c)
     })
-  function click_player(element,side_c)
-  {
+   function click_player(element,side_c)
+   {
     if (control_player())
     {
       game_st.Color_order_player.push(side_c.value)
-      game_st.control_player=false
       element.style.background =side_c.highlight_color
       
       setTimeout(()=>{
-       game_st.control_player=true
        element.style.background =side_c.Initial_color
        console.log(game_st)
        if (game_st.Color_order.length==game_st.Color_order_player.length)
        { 
          console.log (check_result())
          if( check_result())
-         { 
-            if (game_st.Color_order.length==1)
-            {
-              feedback.innerHTML='הדסה  ברעוז'
-            round_two()
-           .then(()=>{
-            game_st.control_player=true
-             })
-            }
-            else if(game_st.Color_order.length==2)
-             {
-              feedback.innerHTML='הדסה אלופה'
-             round_three() 
-             }
-         else feedback.innerHTML='הדסה ענקיתתת'
-        }
-       else
-       feedback.innerHTML='נכשלת '
+         {
+          Feedback()
+          game_st.Color_order_player.length=0
+         add_new_color()
+         }
+         else
+         feedback.innerHTML='נסה שוב חבר'
       } },500)  
     }
   }
+  function Feedback()
+  { 
+    switch (game_st.Color_order.length) {
+      case 1:
+          feedback.innerHTML='קל מידי '
+          break;
+      case 2:
+        feedback.innerHTML='עדיין קל'
+          break;
+      case 3:
+        feedback.innerHTML='יפה לך'
+          break;
+          case 4:
+        feedback.innerHTML='הופה התחלנו'
+          break;
+      case 5:
+        feedback.innerHTML='ענק.תמשיך לבד'
+        break;
+  }
+}
+
+  
   function control_player()
   {
     if (game_st.control_player&& game_st.Color_order.length>game_st.Color_order_player.length)
     return true
   } 
-function init_status(){
+ function init_status(){
  game_st.Color_order.length=0
  game_st.Color_order_player.length=0
  game_st.control_player=false
-} 
+ } 
     function check_result(){
       for(let i=0;i<game_st.Color_order.length;i++)
       {
@@ -117,42 +126,57 @@ function init_status(){
       return true 
     }
       start.addEventListener('click', () => {
-        round_one()
-       .then(()=>{
-         game_st.control_player=true
-         console.log(game_st)
-        })
-       })
-
-       function round_one(){
-        return new Promise((resolve, reject) => {
         init_status()
-        toggleColor(top_left_c,t_l)
-        .then(()=>
-        resolve())
+        add_new_color()
         })
+  
+  
+       function next_round(num){ 
+        return new Promise((resolve, reject) => {
+        if (num==1)
+       { 
+        toggleColor(top_left_c,t_l)
+        .then (()=>   resolve())
+       }
+        else if (num==2) 
+        { 
+        toggleColor(top_right_c,t_r)
+        .then (()=>   resolve())
+        }
+        else if (num==3)
+        {
+        toggleColor(bottom_left_c,b_l)
+        .then (()=>   resolve())
+        }
+        else
+        {
+        toggleColor(bottom_right_c,b_r)
+        .then (()=>   resolve())
+   
+      }
+        })
+      } 
+       function add_new_color()
+       {
+        game_st.control_player=false
+        num= Math.floor(Math.random() * 4)+ 1;
+        game_st.Color_order.push(num)
+        computer_control()
        }
 
-       function round_two(){
-        return new Promise((resolve, reject) => {
-          round_one()
-          .then(()=>
-            toggleColor(bottom_right_c,b_r)
-          )
-            .then(()=>{
-              resolve()
-          })     
-        })
+      computer_control = async () => {
+        for ( let i=0;i<game_st.Color_order.length;i++)
+       await next_round(game_st.Color_order[i])
+       game_st.control_player=true
+       console.log(game_st.control_player)
       }
-      function round_three(){
-        game_st.control_player=false
-        round_two()
-        .then(()=>
-          toggleColor(bottom_left_c,b_l) )
-        .then(()=>
-          game_st.control_player=true
-        )
-      }
+
+      
+ 
+  
+
+ 
+    
 
      
 
